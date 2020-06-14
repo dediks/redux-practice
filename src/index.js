2096868af654a2df733ef7f9b69d5069b159627f
Mark Erikson <mark@isquaredsoftware.com>
2020-06-14 15:59:11 -0400

    Fetch a list of users from the server on startup

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ import { Provider } from 'react-redux'
 
 import { worker } from './api/server'
 
+import { fetchUsers } from './features/users/usersSlice'
+
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
+
+  store.dispatch(fetchUsers())
 
   ReactDOM.render(
     <React.StrictMode>
