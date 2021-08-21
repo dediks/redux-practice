5ecb9828c09a5b41a169106fb0f7f88f8bcfd8db
Mark Erikson <mark@isquaredsoftware.com>
2021-08-21 14:07:19 -0400

    Extract getUsers into an injected endpoint

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import { Provider } from 'react-redux'
 
 import { worker } from './api/server'
 
-import { apiSlice } from './features/api/apiSlice'
+import { extendedApiSlice } from './features/users/usersSlice'
 
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
 
-  store.dispatch(apiSlice.endpoints.getUsers.initiate())
+  store.dispatch(extendedApiSlice.endpoints.getUsers.initiate())
 
   ReactDOM.render(
     <React.StrictMode>
